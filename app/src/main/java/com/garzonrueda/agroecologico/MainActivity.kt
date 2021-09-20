84d000d46957a7432ee995750ffe08c0013f7983
Andy96NNN <julian.andres.garzon@correounivalle.edu.co>
2021-09-20 14:16:16 -0500

    Correcciones Semana 1

--- app/src/main/java/com/garzonrueda/agroecologico/MainActivity.kt
+++ app/src/main/java/com/garzonrueda/agroecologico/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import com.garzonrueda.agroecologico.databinding.ActivityMainBinding
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
@@ -26,6 +28,29 @@ class MainActivity : AppCompatActivity() {
 
         viewBinding.button5.setOnClickListener {
             goSellerLogin()
+        }
+
+        viewBinding.btnSearchRole.setOnClickListener {
+
+            val input = viewBinding.etUsername.text
+
+            if (input.isNotEmpty()) {
+
+                val db = FirebaseDatabase.getInstance().reference
+
+                db.child("Users").child(input.toString()).get().addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Toast.makeText(this, it.result?.child("rol")?.value.toString(), Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "Usuario no encontrado", Toast.LENGTH_SHORT).show()
+                    }
+                }.addOnFailureListener {
+                    Toast.makeText(this, "fallido", Toast.LENGTH_SHORT).show()
+                }
+
+            } else {
+                Toast.makeText(this, "Ingrese nombre de usuario", Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
--- app/src/main/java/com/garzonrueda/agroecologico/SalesPointActivity.kt
+++ app/src/main/java/com/garzonrueda/agroecologico/SalesPointActivity.kt
@@ -3,7 +3,9 @@ package com.garzonrueda.agroecologico
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import com.garzonrueda.agroecologico.databinding.ActivitySalesPointBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
 class SalesPointActivity : AppCompatActivity() {
@@ -25,8 +27,52 @@ class SalesPointActivity : AppCompatActivity() {
 
         title = "AgroEcologico - Crear Punto de Venta"
 
+        val db = FirebaseDatabase.getInstance().reference
+
         viewBinding.backButton.setOnClickListener {
             onBackPressed()
         }
+
+        viewBinding.btnCreate.setOnClickListener {
+            if (
+                viewBinding.etPointName.text.isNotEmpty()
+                && viewBinding.etSellerId.text.isNotEmpty()
+                && viewBinding.etSellerPwd.text.isNotEmpty()
+                && viewBinding.etPhone.text.isNotEmpty()
+                && viewBinding.etSellerEmail.text.isNotEmpty()
+            ) {
+                db.child("Users").child(viewBinding.etSellerId.text.toString()).child("name").setValue(viewBinding.etPointName.text.toString())
+                db.child("Users").child(viewBinding.etSellerId.text.toString()).child("password").setValue(viewBinding.etSellerPwd.text.toString())
+                db.child("Users").child(viewBinding.etSellerId.text.toString()).child("phone").setValue(viewBinding.etPhone.text.toString())
+                db.child("Users").child(viewBinding.etSellerId.text.toString()).child("email").setValue(viewBinding.etSellerEmail.text.toString())
+                db.child("Users").child(viewBinding.etSellerId.text.toString()).child("rol").setValue("seller")
+
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    viewBinding.etSellerEmail.text.toString(),
+                    viewBinding.etSellerPwd.text.toString()
+                ).addOnCompleteListener {
+
+                    Toast.makeText(this, "Punto de venta regsitrado", Toast.LENGTH_LONG).show()
+                    clearFields()
+
+                }.addOnFailureListener {
+
+                    Toast.makeText(this, "Registro Fallido", Toast.LENGTH_LONG).show()
+                    viewBinding.etPointName.requestFocus()
+                }
+
+
+            } else {
+                Toast.makeText(this, "Rellene todos los campos!", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun clearFields() {
+        viewBinding.etPhone.text.clear()
+        viewBinding.etPointName.text.clear()
+        viewBinding.etSellerEmail.text.clear()
+        viewBinding.etSellerId.text.clear()
+        viewBinding.etSellerPwd.text.clear()
     }
 }
